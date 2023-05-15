8b1a3c304611e3b332f87584a0a5c54643610e3a
cesarjakub <list>
2023-05-15 21:37:39 +0200

    feat: javascript card

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     // API -> https://pokeapi.co/api/v2/pokemon/ditto
 
+    function card(img, name, rank){
+        let text = `
+        <div class="card m-3 mb-4" style="width: 18rem;">
+        <img src="${img}" class="card-img-top" alt="${name}">
+            <div class="card-body">
+                <h5 class="card-title">Name> ${name}</h5>
+                <p class="card-text">Rank> ${rank}</p>
+                <button type="button" class="btn btn-primary"><img src="../src/svg/star.svg" alt="star"></button>
+            </div>
+        </div>`;
+        return text;
+    }
 
     function getIDFromAPI(id){
         $.ajax({
