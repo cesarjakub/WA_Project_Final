053af47507a22974766d277a02b3ff621219cd74
cesarjakub <list>
2023-05-17 20:43:13 +0200

    feat: pokemons json file

---
+++ javascripts/pokedex.js
@@ -0,0 +1,11 @@
+import pokemons from './json/pokemons.json' assert {type: 'json'};
+
+$(document).ready(function(){
+
+    let pokePole = [];
+    console.log(pokemons);
+    
+    console.log(pokePole);
+    getIDs();
+
+});
