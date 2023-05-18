5c85761db9d6bbd7a395136a08df980427f4ea76
cesarjakub <list>
2023-05-18 16:21:30 +0200

    feat: json uprava

--- javascripts/pokedex.js
+++ javascripts/pokedex.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
     //API -> https://raw.githubusercontent.com/cesarjakub/WA_Project_Final/master/json/pokemons.json
-    
+    let pokemonsPole = [];
+
     function getIDs(){
         $.ajax({
-            url: `https://raw.githubusercontent.com/cesarjakub/WA_Project_Final/master/json/pokemons.json`,
-            type: "GET",
-            data: {
-            },
-            success: function (result) {
-              console.log(result["pokemons"]);
+            url: "https://raw.githubusercontent.com/cesarjakub/WA_Project_Final/master/json/pokemons.json",
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                pokemonsPole.push(data);
             },
             error: function (error) {
                 console.log(error);
-            },
+            }
         });
     }
+    console.log(pokemonsPole);
     getIDs();
 
 });
